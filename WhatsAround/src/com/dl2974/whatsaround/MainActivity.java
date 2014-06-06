a14eb3592b6db0992d6b3c6bfd37e27be10f080a
DLubin <DLubin@ABO007.welco.local>
2014-06-06 18:20:59 -0400

    June 6 6:19PM

--- WhatsAround/src/com/dl2974/whatsaround/MainActivity.java
+++ WhatsAround/src/com/dl2974/whatsaround/MainActivity.java
@@ -1,12 +1,14 @@
 package com.dl2974.whatsaround;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
@@ -18,11 +20,14 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements 
 LocationsListFragment.OnLocationTypeSelectedListener, 
 FactualFragment.OnLocationSelectedListener,
-LocationFragment.MapListener {
+LocationFragment.MapListener,
+GoogleMap.InfoWindowAdapter {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -79,13 +84,38 @@ LocationFragment.MapListener {
         //GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.gmap)).getMap();
         LatLng locationLongLat = new LatLng( Double.valueOf(locationData.get("latitude")), Double.valueOf(locationData.get("longitude")) );
 
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String,String> entry : locationData.entrySet()){
+        	sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
+        }
+        String markerSnippet = sb.toString();
+        
         //map.setMyLocationEnabled(true);
         //map.moveCamera(CameraUpdateFactory.newLatLngZoom(locationLongLat, 13));
-         map.addMarker(new MarkerOptions().position(locationLongLat).title(locationData.get("name")));
+         Marker marker = map.addMarker(new MarkerOptions().position(locationLongLat).title(locationData.get("name")).snippet(markerSnippet));
+         map.setInfoWindowAdapter(this);
+         marker.showInfoWindow();
          map.moveCamera(CameraUpdateFactory.newLatLngZoom(locationLongLat, 15));
          map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
          
 	}
+	
+	
+	public View getInfoWindow(Marker marker){
+		
+		return null;
+	}	
+	
+	public View getInfoContents(Marker marker){
+		
+		View infoWindowView =  getLayoutInflater().inflate(R.layout.info_window, null);
+		TextView textData = (TextView) infoWindowView.findViewById(R.id.location_info);
+		textData.setText(marker.getSnippet());
+		
+		return infoWindowView;
+		
+	}
+	
 	
 	/*
 	@Override
