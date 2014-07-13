72d2729915550ac7ec74ef7002acd58fccc926bc
Dan <danl2974@msn.com>
2014-07-12 23:32:35 -0400

    July 12 11:31PM

--- WhatsAround/src/com/dl2974/whatsaround/PlacesClient.java
+++ WhatsAround/src/com/dl2974/whatsaround/PlacesClient.java
@@ -38,7 +38,12 @@ public class PlacesClient {
     private String endpoint;
     private String requestParameters;
     private PlacesCallType placesCallType;
-
+    public static String[] searchDataFields = {"icon","id","name","place_id","reference","scope","vicinity"};
+    public static String[] detailsDataFields = {"formatted_address","formatted_phone_number","icon","id","scope","url","user_ratings_total","utc_offset","vicinity","website","place_id","price_level","reference"};
+    public static String[] reviewsDataFields = {"author_name","author_url","language","rating","text","time"};
+    public static String[] photosDataFields = {};
+    public static String[] days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    
 	public enum PlacesCallType {
 		search,
 		details,
@@ -73,10 +78,10 @@ public class PlacesClient {
 		  
 	}
 	
-	public ArrayList<HashMap<String,String>> getPlacesData(){
+	public ArrayList<HashMap<String,Object>> getPlacesData(){
 		
-		ArrayList<HashMap<String,String>> result = null;
-		AsyncTask<String, Void, ArrayList<HashMap<String,String>>> task = new PlacesClientTask().execute(this.requestParameters);
+		ArrayList<HashMap<String,Object>> result = null;
+		AsyncTask<String, Void, ArrayList<HashMap<String,Object>>> task = new PlacesClientTask().execute(this.requestParameters);
 		   
 		try {
 			 result = task.get();
@@ -139,15 +144,15 @@ public class PlacesClient {
 	
 	
 	
-    public class PlacesClientTask extends AsyncTask<String, Void, ArrayList<HashMap<String,String>> > {
+    public class PlacesClientTask extends AsyncTask<String, Void, ArrayList<HashMap<String,Object>> > {
 	    	 
 	        @Override
-	        protected ArrayList<HashMap<String,String>> doInBackground(String... urlParams) {
+	        protected ArrayList<HashMap<String,Object>> doInBackground(String... urlParams) {
 	              
 	        try {
 	        	
                     String placesResult = call(PlacesClient.this.endpoint, urlParams[0]);
-                    ArrayList<HashMap<String,String>> response = null;
+                    ArrayList<HashMap<String,Object>> response = null;
                     
             		switch(PlacesClient.this.placesCallType){
             		
@@ -175,7 +180,7 @@ public class PlacesClient {
 	        	        
 	        // onPostExecute displays the results of the AsyncTask.
 	        @Override
-	        protected void onPostExecute(ArrayList<HashMap<String,String>> result) {
+	        protected void onPostExecute(ArrayList<HashMap<String,Object>> result) {
                 //No UI to update      	
 	        	
 	       }
@@ -198,143 +203,168 @@ public class PlacesClient {
     }
     
     
-    ArrayList<HashMap<String,String>>  parseJsonSearchResponse(String jsonString){
+    ArrayList<HashMap<String,Object>>  parseJsonSearchResponse(String jsonString){
     	
-		ArrayList<HashMap<String,String>> hmlist = new ArrayList<HashMap<String,String>>();
+		ArrayList<HashMap<String,Object>> hmlist = new ArrayList<HashMap<String,Object>>();
 		JSONParser parser= new JSONParser();
 		JSONObject obj = null;
         if(jsonString != null){
 		  try {
 			obj = (JSONObject) parser.parse(jsonString);
 		  } catch (ParseException e) {
-			Log.e("parseJsonResponse", e.getMessage());
+			Log.e("parseJsonSearchResponse", e.getMessage());
 		  }
         
-		  JSONObject responseObj = (JSONObject) obj.get("response");
-		  //JSONObject dataObj = (JSONObject) responseObj.get("data");
-		  //Set<String> keys = dataObj.keySet();
-		  JSONArray dataArray = (JSONArray) responseObj.get("data");
-		  Iterator i = dataArray.iterator();
-
-		  while (i.hasNext()) {
-			  HashMap<String,String> hm = new HashMap<String,String>();
-			  JSONObject dataitem = (JSONObject) i.next();
-			  for(int j = 0; j < FactualLocation.StringFields.length; j++){
-				if (dataitem.get(FactualLocation.StringFields[j]) != null){ 
-				  Log.i("Parser",  FactualLocation.StringFields[j] + (String) dataitem.get(FactualLocation.StringFields[j]) );
-			      hm.put(FactualLocation.StringFields[j], (String) dataitem.get(FactualLocation.StringFields[j]));
-				}else{Log.i("NullParser",  FactualLocation.StringFields[j]);}
+		  if (obj.get("status").equals("OK")){
+		  
+			  JSONArray resultsArray = (JSONArray) obj.get("results");
+			  Iterator i = resultsArray.iterator();
+			  while (i.hasNext()) {
+				  
+				  HashMap<String,Object> hm = new HashMap<String,Object>();
+				  JSONObject resultItem = (JSONObject) i.next();
+				  JSONObject location = (JSONObject) ((JSONObject) resultItem.get("geometry")).get("location");
+				  hm.put("latitude", String.valueOf(location.get("lat")));
+				  hm.put("longitude", String.valueOf(location.get("lng")));
+				  JSONArray types = (JSONArray) resultItem.get("types");
+				  ArrayList typesList = new ArrayList();
+				  for(int t = 0; t < types.size(); t++){
+					  typesList.add(types.get(t));
+				  }
+				  hm.put("types",typesList);
+				  for(int f = 0; f < PlacesClient.searchDataFields.length; f++){
+					  String field = PlacesClient.searchDataFields[f];
+					  hm.put(field, (String) resultItem.get(field));
+				  }	
+				  
+			   hmlist.add(hm);
 			  }
-			  
-			  for(int j = 0; j < FactualLocation.DoubleFields.length; j++){
-				if (dataitem.get(FactualLocation.DoubleFields[j]) != null){ 
-				  //Log.i("Parser",  FactualLocation.DoubleFields[j] + (String) dataitem.get(FactualLocation.DoubleFields[j]) );
-			      hm.put(FactualLocation.DoubleFields[j], String.valueOf(dataitem.get(FactualLocation.DoubleFields[j]))  );
-				}else{Log.i("NullParser",  FactualLocation.DoubleFields[j]);}
-			  }			  
-			  
-			  hmlist.add(hm);
+		  
 		  }
-        
         }
 		return hmlist;    	
-    	
     	
     } 
     
     
     
-    ArrayList<HashMap<String,String>>  parseJsonDetailsResponse(String jsonString){
-    	
-  		ArrayList<HashMap<String,String>> hmlist = new ArrayList<HashMap<String,String>>();
-  		JSONParser parser= new JSONParser();
-  		JSONObject obj = null;
-          if(jsonString != null){
-  		  try {
-  			obj = (JSONObject) parser.parse(jsonString);
-  		  } catch (ParseException e) {
-  			Log.e("parseJsonResponse", e.getMessage());
-  		  }
-          
-  		  JSONObject responseObj = (JSONObject) obj.get("response");
-  		  //JSONObject dataObj = (JSONObject) responseObj.get("data");
-  		  //Set<String> keys = dataObj.keySet();
-  		  JSONArray dataArray = (JSONArray) responseObj.get("data");
-  		  Iterator i = dataArray.iterator();
-
-  		  while (i.hasNext()) {
-  			  HashMap<String,String> hm = new HashMap<String,String>();
-  			  JSONObject dataitem = (JSONObject) i.next();
-  			  for(int j = 0; j < FactualLocation.StringFields.length; j++){
-  				if (dataitem.get(FactualLocation.StringFields[j]) != null){ 
-  				  Log.i("Parser",  FactualLocation.StringFields[j] + (String) dataitem.get(FactualLocation.StringFields[j]) );
-  			      hm.put(FactualLocation.StringFields[j], (String) dataitem.get(FactualLocation.StringFields[j]));
-  				}else{Log.i("NullParser",  FactualLocation.StringFields[j]);}
-  			  }
-  			  
-  			  for(int j = 0; j < FactualLocation.DoubleFields.length; j++){
-  				if (dataitem.get(FactualLocation.DoubleFields[j]) != null){ 
-  				  //Log.i("Parser",  FactualLocation.DoubleFields[j] + (String) dataitem.get(FactualLocation.DoubleFields[j]) );
-  			      hm.put(FactualLocation.DoubleFields[j], String.valueOf(dataitem.get(FactualLocation.DoubleFields[j]))  );
-  				}else{Log.i("NullParser",  FactualLocation.DoubleFields[j]);}
-  			  }			  
-  			  
-  			  hmlist.add(hm);
-  		  }
-          
-          }
-  		return hmlist;    	
-      	
-      	
+    ArrayList<HashMap<String,Object>>  parseJsonDetailsResponse(String jsonString){
+    	 	
+		ArrayList<HashMap<String,Object>> hmlist = new ArrayList<HashMap<String,Object>>();
+		JSONParser parser= new JSONParser();
+		JSONObject obj = null;
+        if(jsonString != null){
+		  try {
+			obj = (JSONObject) parser.parse(jsonString);
+		  } catch (ParseException e) {
+			Log.e("parseJsonDetailsResponse", e.getMessage());
+		  }
+		  
+		  if (obj.get("status").equals("OK")){
+			  
+			  JSONObject resultObj = (JSONObject) obj.get("result");
+			  HashMap<String,Object> hm = new HashMap<String,Object>();
+			  
+			  for(int f = 0; f < PlacesClient.detailsDataFields.length; f++){
+				  String field = PlacesClient.detailsDataFields[f];
+				  hm.put(field, (String) resultObj.get(field));
+			  }
+			  
+			  hm.put("hours", formatDetailsHours((JSONObject) resultObj.get("opening_hours")));
+			  hm.put("photos", formatDetailsPhotos((JSONArray) resultObj.get("photos")));
+			  hm.put("reviews", formatDetailsReviews((JSONArray) resultObj.get("reviews")));
+			  
+		  }
+		  
+        }
+        
+        return hmlist;
+        
       } 
     
     
     
     
-    ArrayList<HashMap<String,String>>  parseJsonPhotosResponse(String jsonString){
-    	
-  		ArrayList<HashMap<String,String>> hmlist = new ArrayList<HashMap<String,String>>();
-  		JSONParser parser= new JSONParser();
-  		JSONObject obj = null;
-          if(jsonString != null){
-  		  try {
-  			obj = (JSONObject) parser.parse(jsonString);
-  		  } catch (ParseException e) {
-  			Log.e("parseJsonResponse", e.getMessage());
-  		  }
-          
-  		  JSONObject responseObj = (JSONObject) obj.get("response");
-  		  //JSONObject dataObj = (JSONObject) responseObj.get("data");
-  		  //Set<String> keys = dataObj.keySet();
-  		  JSONArray dataArray = (JSONArray) responseObj.get("data");
-  		  Iterator i = dataArray.iterator();
+    ArrayList<HashMap<String,Object>>  parseJsonPhotosResponse(String jsonString){
 
-  		  while (i.hasNext()) {
-  			  HashMap<String,String> hm = new HashMap<String,String>();
-  			  JSONObject dataitem = (JSONObject) i.next();
-  			  for(int j = 0; j < FactualLocation.StringFields.length; j++){
-  				if (dataitem.get(FactualLocation.StringFields[j]) != null){ 
-  				  Log.i("Parser",  FactualLocation.StringFields[j] + (String) dataitem.get(FactualLocation.StringFields[j]) );
-  			      hm.put(FactualLocation.StringFields[j], (String) dataitem.get(FactualLocation.StringFields[j]));
-  				}else{Log.i("NullParser",  FactualLocation.StringFields[j]);}
-  			  }
-  			  
-  			  for(int j = 0; j < FactualLocation.DoubleFields.length; j++){
-  				if (dataitem.get(FactualLocation.DoubleFields[j]) != null){ 
-  				  //Log.i("Parser",  FactualLocation.DoubleFields[j] + (String) dataitem.get(FactualLocation.DoubleFields[j]) );
-  			      hm.put(FactualLocation.DoubleFields[j], String.valueOf(dataitem.get(FactualLocation.DoubleFields[j]))  );
-  				}else{Log.i("NullParser",  FactualLocation.DoubleFields[j]);}
-  			  }			  
-  			  
-  			  hmlist.add(hm);
-  		  }
-          
-          }
-  		return hmlist;    	
+		ArrayList<HashMap<String,Object>> hmlist = new ArrayList<HashMap<String,Object>>();
+		JSONParser parser= new JSONParser();
+		JSONObject obj = null;
+        if(jsonString != null){
+		  try {
+			obj = (JSONObject) parser.parse(jsonString);
+		  } catch (ParseException e) {
+			Log.e("parseJsonSearchResponse", e.getMessage());
+		  }
+		  
+		  
+		  
+		  
+        }
+        
+        return hmlist;
       	
       	
       }     
     
+    
+    
+    private String formatDetailsHours(JSONObject jsonObj){
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+    	Boolean opened = (Boolean) jsonObj.get("open_now");
+    	String businessState = opened ? "Opened Now": "Closed Now";
+    	sb.append(businessState + "\n");
+    	
+    	JSONArray periods = (JSONArray) jsonObj.get("periods");
+    	Iterator iter = periods.iterator();
+    	while(iter.hasNext()){
+    		JSONObject dayOfWeek = (JSONObject) iter.next();
+    		int dayIndex = (Integer) ((JSONObject) dayOfWeek.get("open")).get("day");
+    		String open = (String) ((JSONObject) dayOfWeek.get("open")).get("time");
+    		String openStr = Integer.valueOf(open.substring(0,2)) > 12 ? (String.valueOf(Integer.valueOf(open.substring(0,2)) - 12) + ":" + open.substring(2,2)) : (open.substring(0,2) + ":" + open.substring(2,2));
+    		String close = (String) ((JSONObject) dayOfWeek.get("close")).get("time");
+    		String closeStr = Integer.valueOf(close.substring(0,2)) > 12 ? (String.valueOf(Integer.valueOf(close.substring(0,2)) - 12) + ":" + close.substring(2,2)) : (close.substring(0,2) + ":" + close.substring(2,2));
+    		sb.append(String.format("%s: %s - %s\n", PlacesClient.days[dayIndex], openStr, closeStr));
+    	}
+    	
+    	return sb.toString();
+    }
+    
+    
+    private ArrayList<HashMap<String,Object>> formatDetailsPhotos(JSONArray jsonArr){
+    	
+    	ArrayList<HashMap<String,Object>> hmlist = new ArrayList<HashMap<String,Object>>();
+    	Iterator iter = jsonArr.iterator();
+    	while(iter.hasNext()){
+    		JSONObject photos = (JSONObject) iter.next();
+    		HashMap<String,Object> hm = new HashMap<String,Object>();
+    		hm.put("photo_reference", (String) photos.get("photo_reference"));
+    		hm.put("width", (Integer) photos.get("width"));
+    		hm.put("height", (Integer) photos.get("height"));
+    		hmlist.add(hm);
+    	}
+    	return hmlist;
+    }
+    
+    private ArrayList<HashMap<String,Object>> formatDetailsReviews(JSONArray jsonArr){
+    	
+    	ArrayList<HashMap<String,Object>> hmlist = new ArrayList<HashMap<String,Object>>();
+    	Iterator iter = jsonArr.iterator();
+    	while(iter.hasNext()){
+    		JSONObject reviews = (JSONObject) iter.next();
+    		if ( ((String) ((JSONObject) ((JSONArray) reviews.get("aspects")).get(0)).get("type")).equals("quality") ){
+    		   HashMap<String,Object> hm = new HashMap<String,Object>();
+    		   for(int i = 0; i < PlacesClient.reviewsDataFields.length; i++){
+    			   hm.put(PlacesClient.reviewsDataFields[i], (String) reviews.get(PlacesClient.reviewsDataFields[i]));
+    		   }
+    		   hmlist.add(hm);
+    		}
+    	}
+    	return hmlist;    	
+    	
+    }
     
     
 }
